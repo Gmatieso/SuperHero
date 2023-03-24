5b77528870fabe9f20d790d83a5dce7d98cb2881
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-24 16:27:27 +0300

    Created HerosItem Composable

--- app/src/main/java/com/example/superhero/MainActivity.kt
+++ app/src/main/java/com/example/superhero/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.superhero
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.superhero.model.Hero
 import com.example.superhero.ui.theme.SuperHeroTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +38,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SuperHeroApp() {
 
+}
+
+@Composable
+fun HerosItem(hero: Hero, modifier: Modifier = Modifier) {
+    Card(
+        elevation = 4.dp,
+        modifier = modifier.padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            //Other Composables go here
+            /*TODO*/
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
