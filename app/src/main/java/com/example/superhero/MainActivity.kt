6eb2524133a400e13d0be2be3fc123b4c2f0562e
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-25 04:19:47 +0300

    Created SuperHeroTopAppBar composables

--- app/src/main/java/com/example/superhero/MainActivity.kt
+++ app/src/main/java/com/example/superhero/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -59,10 +60,26 @@ fun SuperHeroList( modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun SuperHeroTopAppBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colors.primary),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h1
+        )
+    }
+
+}
+
+@Composable
 fun HeroesItem(hero: Hero, modifier: Modifier = Modifier) {
     Card(
-        elevation = 4.dp,
-        modifier = modifier.padding(8.dp)
+        elevation = 2.dp,
+        modifier = modifier.padding(16.dp)
     ) {
         Row(
             modifier = Modifier
@@ -72,7 +89,7 @@ fun HeroesItem(hero: Hero, modifier: Modifier = Modifier) {
             //Other Composables go here
             /*TODO*/
             HeroInformation(hero.nameRes,hero.descriptionRes )
-            Spacer(Modifier.weight(1f))
+            Spacer(modifier = Modifier.width(16.dp))
             HeroIcon(hero.imageRes)
 
 
@@ -88,7 +105,7 @@ fun HeroInformation(@StringRes heroName: Int, @StringRes heroDescription: Int, m
         //heroName Composable
         Text(
             text = stringResource(heroName),
-            style = MaterialTheme.typography.h2,
+            style = MaterialTheme.typography.h3,
             modifier = modifier.padding(8.dp)
         )
         //heroDescription Composable
@@ -107,7 +124,7 @@ fun HeroIcon(@DrawableRes heroIcon: Int, modifier: Modifier = Modifier) {
         modifier = modifier
             .size(64.dp)
             .padding(8.dp)
-            .clip(RoundedCornerShape(50)),
+            .clip(RoundedCornerShape(8.dp)),
         contentScale = ContentScale.Crop,
         painter = painterResource(heroIcon),
         contentDescription = null )
